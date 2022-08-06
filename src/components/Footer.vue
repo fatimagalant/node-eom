3fb384015ca115498b6f3a059ede1754b22e7dc9
fatima galantls <fatimagalant03@gmail.com>
2022-08-06 14:37:58 +0200

    footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="Footer">
+    <footer class="bg-dark text-light">
+      <div class="container text-center">
+        <div class="row">
+          <!-- links to other pages -->
+          <div class="col-md-4">Links</div>
+          <!-- Contact -->
+          <div class="col-md-4">Contact</div>
+          <!-- Location -->
+          <div class="col-md-4">Help</div>
+        </div>
+        <!-- Copyright and socials -->
+        <div class="row"></div>
+      </div>
+    </footer>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped></style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,20 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section id="Landing">
+    <h1>text</h1>
+    <Footer />
+  </section>
 </template>
-
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Footer from "../components/Footer.vue";
 export default {
-  name: 'HomeView',
   components: {
-    HelloWorld
-  }
-}
+    Footer,
+  },
+};
 </script>
+<style scoped>
+#Landing {
+  overflow-x: hidden;
+}
+</style>
