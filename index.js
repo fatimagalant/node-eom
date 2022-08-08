bc98ac1eafc238c39ccf58c56541d5b3f956fac0
TristanDaniels <tristandaniels07@gmail.com>
2022-08-08 15:33:25 +0200

    first commit

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require("express"); // Used to set up a server
 const cors = require("cors"); // Used to prevent errors when working locally
 const app = express(); // Initialize express as an app variable
-app.set("port", process.env.PORT || 6969); // Set the port
+// app.set("port", process.env.PORT || 6969); // Set the port
 app.use(express.json()); // Enable the server to handle JSON requests
 app.use(cors()); // Dont let local development give errors
+app.use(express.static("public"));
 app.get("/", (req, res) => {
   res.json({ msg: "Welcome" });
 });
@@ -14,15 +15,17 @@ app.use("/users", userRoute);
 const productRoute = require("./routes/productRoute");
 app.use("/products", productRoute);
 
-app.use(express.static("public"));
 app.get("/", function (req, res) {
   res.sendFile(_dirname + "/" + "login.html");
 });
-app.use(express.static("public"));
 app.get("/", function (req, res) {
   res.sendFile(_dirname + "/" + "register.html");
 });
-app.use(express.static("public"));
 app.get("/", function (req, res) {
   res.sendFile(_dirname + "/" + "reset_password.html");
+});
+
+const port = process.env.PORT || 6969;
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
 });
